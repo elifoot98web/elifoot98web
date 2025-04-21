7d86e7aea8572b4baaf4c0c4ccd75df0e8ac91a7
Leonardo "Leu" Pereira <jlcvp@users.noreply.github.com>
2025-04-21 00:04:26 -0300

    Add web app manifest for Elifoot 98 Online with comprehensive icon set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { MaskitoModule } from '@maskito/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -16,8 +17,13 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [BrowserModule, IonicModule.forRoot({
     mode: 'md' // Use Material Design mode on all platforms
   }), IonicStorageModule.forRoot(),
-    AppRoutingModule, MaskitoModule, HttpClientModule],
+    AppRoutingModule, MaskitoModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
