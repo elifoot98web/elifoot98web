94e0af3a54a09b9f207cd2b8f7abb060c1a75f1a
Leonardo "Leu" Pereira <jlcvp@users.noreply.github.com>
2025-05-11 09:04:22 -0300

    chore: upgrade Angular and related dependencies to version 18.x
    - Updated Angular packages from version 17.3.12 to 18.2.13.
    - Updated Angular DevKit and ESLint packages to their respective 18.x versions.
    - Changed HttpClientModule import to use provideHttpClient with interceptors in app.module.ts.
    - Added "esModuleInterop": true to tsconfig.json for better module compatibility.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MaskitoModule } from '@maskito/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot({
-    mode: 'md' // Use Material Design mode on all platforms
-  }), IonicStorageModule.forRoot(),
-    AppRoutingModule, MaskitoModule, HttpClientModule, ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent],
+    bootstrap: [AppComponent], imports: [BrowserModule, IonicModule.forRoot({
+            mode: 'md' // Use Material Design mode on all platforms
+        }), IonicStorageModule.forRoot(),
+        AppRoutingModule, MaskitoModule, ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })], providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
